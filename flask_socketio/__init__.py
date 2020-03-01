d970765a660df27c74a7798fdfb3f18c9c592d50
root <root@localhost.localdomain>
2020-03-01 22:42:34 +0000

    docker config

--- flask_socketio/__init__.py
+++ flask_socketio/__init__.py
@@ -509,7 +509,7 @@ class SocketIO(object):
                        case.
         """
         if host is None:
-            host = '127.0.0.1'
+            host = '0.0.0.0'
         if port is None:
             server_name = app.config['SERVER_NAME']
             if server_name and ':' in server_name:
