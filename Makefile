d3b1f0e2cc6b3780a044748d4acf6949398dde81
mike <mike@lcd-outlet.com>
2020-03-02 12:27:50 +0000

    download static files

--- Makefile
+++ Makefile
@@ -4,7 +4,11 @@ all:
 	sudo pip3 install tatsu
 	sudo python3 setup.py install
 	sudo service redis-server stop
+	sudo apt install screen
 	rm -rf RedisJSON
 	git clone https://github.com/RedisJSON/RedisJSON.git
-	cd RedisJSON && make && redis-server --loadmodule src/rejson.so &
-
+	cd RedisJSON && make 
+	pwd
+	cd RedisJSON && screen -d -m -S redis_server redis-server --loadmodule src/rejson.so &
+	pwd
+	cd example && /bin/bash start_app.sh
--- example/start_app.sh
+++ example/start_app.sh
@@ -1,2 +1,34 @@
-cd /opt/RedisJSON
-redis-server --loadmodule src/rejson.so bind 0.0.0.0 &
+virtualenv venv -p python3
+source venv/bin/activate
+pip install -r requirements.txt
+#cd /opt/RedisJSON
+#redis-server --loadmodule src/rejson.so bind 0.0.0.0 &
+verify_checksum(){
+	if [[ -f "$1" ]]
+		then
+			cs="$(shasum -b -a 256 "$1" | awk '{ print $1 }' | xxd -r -p | base64)"
+			valid_cs="$(grep -c "$2" <<<"$cs")"
+			echo $cs
+			echo $valid_cs
+			if [[ $valid_cs -eq 1 ]]
+				then
+					echo ok
+				else	
+					echo check file 
+			fi
+
+	fi		
+}
+	
+
+
+
+wget https://code.jquery.com/jquery-1.12.4.min.js -o static/jquery-1.12.4.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js
+
+verify_checksum jquery-1.12.4.min.js "ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+verify_checksum socket.io.js "yr4fRk/GU1ehYJPAs8P4JlTgu0Hdsp4ZKrx8bDEDC3I="
+
+mv jquery-1.12.4.min.js static/
+mv socket.io.js static/
+	
